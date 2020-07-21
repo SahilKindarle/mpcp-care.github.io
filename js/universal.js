0ede913099679ed1b331301f3b4310f5fbcc6759
sahilkindarle <sahilkindarle212@gmail.com>
2020-07-21 14:37:57 +0530

    2 images added ppt

--- js/universal.js
+++ js/universal.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
 
     var elems2 = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems2);
-   
+    var instances = M.Slider.init(elems2,{indicators: false, duration: 2000,interval:2000});
+  
 });
