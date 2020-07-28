cec12632c999bd0419e583536b77499e5a17222b
sahilkindarle <sahilkindarle212@gmail.com>
2020-07-28 14:08:52 +0530

    animation pre loader gif

--- js/universal.js
+++ js/universal.js
@@ -1,8 +1,33 @@
 M.AutoInit();
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
+    loader = document.getElementById('loader');
+    loadNow(1);
 
+    var elems = document.querySelectorAll('.sidenav');
     var elems2 = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems2,{indicators: false, duration: 2000,interval:2000});
-  
+    var instances = M.Slider.init(elems2, {
+        indicators: false,
+        duration: 2000,
+        interval: 2000
+    });
+
 });
+
+
+var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function () {
+            loadNow(opacity - 0.05);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
